5c0f57adc1e5ef1db51c3453108e781b91c40e6c
nvxtien <nvxtien@gmail.com>
2018-10-21 19:36:53 +0700

    remove 'hf'

--- generate.sh
+++ generate.sh
@@ -7,17 +7,23 @@ echo "##########################################################"
 # Define channel name
 export CHANNEL_NAME="exchange"
 # Please change value as below (all value below is example)
+
+# Refer to configtx.yaml
+ORDER_PROFILE="OrgsOrdererGenesis"
+CHANNEL_PROFILE="OrgsChannel"
+
+ORG="PeerOrg1"
+
+SERVICE_NAME="nvxtien"
+DOMAIN="$SERVICE_NAME.io"
+
 ORG_NAME="sale"
-
-NAME="exchange"
-
-DOMAIN="$NAME.io"
+ORG1_DOMAIN="$ORG_NAME.$DOMAIN"
 
 CA_DOMAIN="ca.$ORG_NAME.$DOMAIN"
 
 echo $CA_DOMAIN
 
-ORG1_DOMAIN="$ORG_NAME.$DOMAIN"
 
 ORDERER_DOMAIN="orderer.$DOMAIN"
 
@@ -26,30 +32,34 @@ ORG_MSP="SaleMSP"
 GENESIS_PROFILE="Exchange"
 NETWORK_ID="exchange"
 
-if [ -d "crypto-config" ]; then
-rm -Rf crypto-config
-
-cp configtx.yamlt configtx.yaml
-sed -i "s/DOMAIN/$DOMAIN/g" configtx.yaml
-sed -i "s/ORG_NAME/$ORG_NAME/g" configtx.yaml
-
-cp crypto-config.yamlt crypto-config.yaml
-sed -i "s/DOMAIN/$DOMAIN/g" crypto-config.yaml
-sed -i "s/ORG_NAME/$ORG_NAME/g" crypto-config.yaml
-
-cp docker-compose.yamlt docker-compose.yaml
-sed -i "s/DOMAIN/$DOMAIN/g" docker-compose.yaml
-sed -i "s/ORG_NAME/$ORG_NAME/g" docker-compose.yaml
-sed -i "s/NETWORK_ID/$NETWORK_ID/g" ../docker-compose.yaml
-
-cp ../config.yamlt ../config.yaml
-sed -i "s/DOMAIN/$DOMAIN/g" ../config.yaml
-sed -i "s/ORG_NAME/$ORG_NAME/g" ../config.yaml
-sed -i "s/GENESIS_PROFILE/$GENESIS_PROFILE/g" ../config.yaml
-
+if [ -d "./fixtures/crypto-config" ]; then
+rm -Rf ./fixtures/crypto-config/*
 fi
+
+if [ -d "./fixtures/artifacts" ]; then
+rm -Rf ./fixtures/artifacts/*
+fi
+
+#cp configtx.yamlt configtx.yaml
+#sed -i "s/DOMAIN/$DOMAIN/g" configtx.yaml
+#sed -i "s/ORG_NAME/$ORG_NAME/g" configtx.yaml
+#
+#cp crypto-config.yamlt crypto-config.yaml
+#sed -i "s/DOMAIN/$DOMAIN/g" crypto-config.yaml
+#sed -i "s/ORG_NAME/$ORG_NAME/g" crypto-config.yaml
+#
+#cp docker-compose.yamlt docker-compose.yaml
+#sed -i "s/DOMAIN/$DOMAIN/g" docker-compose.yaml
+#sed -i "s/ORG_NAME/$ORG_NAME/g" docker-compose.yaml
+#sed -i "s/NETWORK_ID/$NETWORK_ID/g" docker-compose.yaml
+#
+#cp ../config.yamlt ../config.yaml
+#sed -i "s/DOMAIN/$DOMAIN/g" ../config.yaml
+#sed -i "s/ORG_NAME/$ORG_NAME/g" ../config.yaml
+#sed -i "s/GENESIS_PROFILE/$GENESIS_PROFILE/g" ../config.yaml
+
 set -x
-cryptogen generate --config=./crypto-config.yaml
+cryptogen generate --config=./crypto-config.yaml --output=./fixtures/crypto-config
 res=$?
 set +x
 if [ $res -ne 0 ]; then
@@ -62,7 +72,7 @@ echo "##########################################################"
 echo "#########  Generating Orderer Genesis block ##############"
 echo "##########################################################"
 set -x
-configtxgen -profile ChainHero -outputBlock ./artifacts/orderer.genesis.block
+configtxgen -profile $ORDER_PROFILE -outputBlock ./fixtures/artifacts/orderer.genesis.block -channelID $CHANNEL_NAME
 res=$?
 set +x
 if [ $res -ne 0 ]; then
@@ -74,7 +84,7 @@ echo "#################################################################"
 echo "### Generating channel configuration transaction 'channel.tx' ###"
 echo "#################################################################"
 set -x
-configtxgen -profile ChainHero -outputCreateChannelTx ./artifacts/"$NAME".channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile $CHANNEL_PROFILE -outputCreateChannelTx ./fixtures/artifacts/"$SERVICE_NAME".channel.tx -channelID $CHANNEL_NAME
 res=$?
 set +x
 if [ $res -ne 0 ]; then
@@ -87,8 +97,7 @@ echo "#################################################################"
 echo "#######    Generating anchor peer update for " + $ORG_NAME +"   ##########"
 echo "#################################################################"
 set -x
-#configtxgen -profile ChainHero -outputAnchorPeersUpdate ./config/${NAME_ORG}anchors.tx -channelID $CHANNEL_NAME -asOrg $NAME_ORG
-configtxgen -profile ChainHero -outputAnchorPeersUpdate ./artifacts/"$ORG_MSP"."$NAME".anchors.tx -channelID $CHANNEL_NAME -asOrg SaleOrg
+configtxgen -profile $CHANNEL_PROFILE -outputAnchorPeersUpdate ./fixtures/artifacts/"$ORG_MSP"."$NAME".anchors.tx -channelID $CHANNEL_NAME -asOrg $ORG
 
 res=$?
 set +x
@@ -112,14 +121,21 @@ fi
 #sed -i "s/ORDERER_DOMAIN/$ORDERER_DOMAIN/g" docker-compose.yml
 #sed -i "s/DOMAIN/$DOMAIN/g" docker-compose.yml
 
+ARCH=$(uname -s | grep Darwin)
+if [ "$ARCH" == "Darwin" ]; then
+OPTS="-it"
+else
+OPTS="-i"
+fi
+
 CURRENT_DIR=$PWD
 
 echo $CURRENT_DIR
 echo $ORG1_DOMAIN
 
-cd ./crypto-config/peerOrganizations/$ORG1_DOMAIN/ca/
+cd ./fixtures/crypto-config/peerOrganizations/$ORG1_DOMAIN/ca/
 echo $PWD
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
 echo $PWD
-sed -i "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yaml
+sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yaml
